456813ecca2f67527bd5a6f62198b0308cbecd8e
SeanTheBuilder1 <68685725+SeanTheBuilder1@users.noreply.github.com>
2020-10-05 11:24:58 +0800

    Standalone exe file for Calcs

--- trianglearea.py
+++ trianglearea.py
@@ -1,13 +1,15 @@
 def Init():
-	while True:
-		try:
-			base = int(input("input the base length of the triangle: "))
-			height = int(input("input the height of the triangle: "))
+    while True:
+        try:
+            base = int(input("input the base length of the triangle: "))
+            height = int(input("input the height of the triangle: "))
 
-			area = (height * base) / 2
+            area = (height * base) / 2
 
-			print('\nthe area of the triangle is: %s\n' % area)
-		except:
-			print("That's not a valid number")
-
-
+            print('\nthe area of the triangle is: %s\n' % area)
+        except:
+            print("That's not a valid number")
+        
+        closer = str(input("Enter 1 to exit.\nPress any key to continue.\n"))
+        if(closer == '1'):
+            return
