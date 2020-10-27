b5bccd982dc341468eceec7dd85173deb7295507
SeanTheBuilder1 <seanthebuilder52@gmail.com>
2020-10-27 12:24:14 +0800

    added sum and updated exe

--- AlgePy.py
+++ AlgePy.py
@@ -1,9 +1,14 @@
 from sympy import solveset, symbols
-import AS_def, AS_sum, GS_def
+import AS_def
+import AS_sum
+import GS_def
+import GS_sum
+
+
 def Init():
     while True:
         try:
-            mode = int(input("ALGEBRA MODE\nPress 1 to use aritmetic sequence definition formula\nPress 2 to use sum of arithmetic sequence formula\nPress 3 to use geometric sequence definition formula\nPress 4 to exit Algebra Mode\n"))
+            mode = int(input("ALGEBRA MODE\nPress 1 to use aritmetic sequence definition formula\nPress 2 to use sum of arithmetic sequence formula\nPress 3 to use geometric sequence definition formula\nPress 4 to use sum of geometric sequence formula\nPress 5 to exit Algebra Mode\n"))
             if mode == 1:
                 value = int(input("Press 1 to get An and generate sequence\nPress 2 to get A1\nPress 3 to get n\nPress 4 to get d\n"))
                 if value == 1:
@@ -16,7 +21,7 @@ def Init():
                     AS_def.calcDefd()
                 else:
                     raise(ValueError)
-				
+
             elif mode == 2:
                 value = int(input("Press 1 to get Sn\nPress 2 to get An\nPress 3 to get A1\nPress 4 to get n\nPress 5 to get d\n"))
                 if value == 1:
@@ -37,9 +42,27 @@ def Init():
                     GS_def.calcDefAn()
                 elif value == 2:
                     GS_def.calcDefA1()
+                elif value == 3:
+                    GS_def.calcDefn()
+                elif value == 4:
+                    GS_def.calcDefr()
+                elif value == 5:
+                    GS_def.calcDefr2()
                 else:
                     raise(ValueError)
             elif mode == 4:
+                value = int(input("Press 1 to get Sn\nPress 2 to get A1\nPress 3 to get r\nPress 4 to get n\n"))
+                if value == 1:
+                    GS_sum.calcSumSn()
+                elif value == 2:
+                    GS_sum.calcSumA1()
+                elif value == 3:
+                    GS_sum.calcSumr()
+                elif value == 4:
+                    GS_sum.calcSumn()
+                else:
+                    raise(ValueError)
+            elif mode == 5:
                 return
             else:
                 raise(ValueError)
